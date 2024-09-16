c4caf2c7a35639c83df9e8e52ba965f7c50249d7
Livya Gomes <livyagomesdc@gmail.com>
2024-09-16 14:58:23 -0300

    Aula 01

---
+++ Aula01.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+//Variáveis, constantes e seus tipos
+
+var inicialNome: Character = "L"
+var nome = "Livya"
+var idade = 19
+var altura = "1,64cm"
+var gostaSwift = true
+
+
+print("""
+Inicial do nome: \(inicialNome)
+Nome: \(nome)
+Idade: \(idade)
+Altura: \(altura)
+Gosta de swift: \(gostaSwift)
+""")
---
+++ Desafio02.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+// Desafio 02 - Cálculo de média
+
+// Crie três variáveis que representam três notas de diferentes alunos. A seguir, crie uma variável que calcule a média dessas notas e mostre essa média no console.
+var aluno01 = 8.5
+var aluno02 = 8.0
+var aluno03 = 9.0
+
+var media = (aluno01 + aluno02 + aluno03)/3
+
+print("A média dos alunos é igual a: \(media)")
