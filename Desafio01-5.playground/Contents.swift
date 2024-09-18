7c30b211d32024f0d8f89c31cb680fc2739ea0ac
Livya Gomes <livyagomesdc@gmail.com>
2024-09-18 13:33:13 -0300

    Aula 05

--- Desafio01-5.playground/Contents.swift
+++ Desafio01-5.playground/Contents.swift
@@ -1,4 +1,4 @@
-//Desafio 01: Aprovao ou Reprovado?
+//Desafio 01: Aprovado ou Reprovado?
 
 //Escreva uma função que receba um array de notas de um determinado aluno e retorne a média aritmética dessas notas.
 
@@ -14,3 +14,30 @@ func calcularMedia(_ notas: Array<Double>) {
 
 calcularMedia([5.5, 6.7, 8.8])
 
+
+func calcularMedia2(_ notas: Array<Double>) {
+  var soma = 0.0
+  
+  for i in 0..<notas.count {
+    soma += notas[i]
+  }
+  var media = soma / Double(notas.count)
+  
+  print(media)
+}
+let notas = [9.0, 7.5, 8.5, 9.5, 10]
+calcularMedia2(notas)
+
+/* Resposta
+func calculaMedia(_ notas: [Double]) -> Double {
+    var somaNotas = 0.0
+    let quantidadeElementos = notas.count
+    for nota in notas {
+        somaNotas += nota
+    }
+    return somaNotas / Double(quantidadeElementos)
+}
+let notas = [8.9, 10, 9.5, 6.7, 8.5]
+let mediaNotas = calculaMedia(notas)
+print(mediaNotas)
+*/
--- Desafio02-5.playground/Contents.swift
+++ Desafio02-5.playground/Contents.swift
@@ -1,5 +1,26 @@
-// Desafio 02: Estados do Brasil
+/*
+Desafio 2 - Estados do Brasil
+Dado um dicionário com duas letras que representam um estado como chave e o nome inteiro do estado como valor, escreva uma
+função que exiba todos os estados cujo nome tenha mais que 8 caracteres.
+*/
 
+func tamanhoEstados(_ estados: Dictionary<String, String>) {
+  for estado in estados.values {
+    
+    if estado.count > 8 {
+      print(estado)
+    }
+  }
+}
+let estados: [String: String] = [
+    "PE": "Pernambuco",
+    "SP": "Sao Paulo",
+    "RJ": "Rio de Janeiro",
+    "CE": "Ceara"
+  ]
+tamanhoEstados(estados)
+
+//Resposta
 func exibeEstado(_ estados: [String: String]) {
     for estado in estados.values {
         if estado.count > 8 {
@@ -7,11 +28,4 @@ func exibeEstado(_ estados: [String: String]) {
         }
     }
 }
-
-let estados = [
-    "PE": "Pernambuco",
-    "SP": "São Paulo",
-    "CE": "Ceará",
-    "RJ": "Rio de Janeiro"
-]
 exibeEstado(estados)
